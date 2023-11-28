6bc1ce62f3f64c0c4463c669fc79762ec6b75e4d
Ritik303 <rtk3032002@gmail.com>
2023-11-28 22:39:02 +0530

    commit tewelve

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,9 @@
 import React from "react";
-const Footer=()=>{
-    return(
-        <div className="container-fluid d-flex justify-content-center align-items-center bg-dark text-white p-3">
-        <h5> Developed by <i>Mini-Project group</i></h5></div>
-  )
+
+const Footer = () => {
+  return (
+   <div className="d-flex justify-content-center align-items-center"style={{backgroundColor:"black",color:"white",height:"150px"}}><h3>© 2023 Copyright all rights reserved :</h3><i><h2>BLINK News</h2></i></div>
+  );
 };
+
 export default Footer;
